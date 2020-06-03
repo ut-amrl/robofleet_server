7b5afc586dae7569984acf5747724734b67a48fb
Logan Zartman <logan.zartman@utexas.edu>
2020-06-03 14:28:39 -0500

    basic echo server

---
+++ README.md
@@ -0,0 +1,12 @@
+# robofleet_server
+
+*RoboFleet 2.0 Server*
+
+## Dependencies
+
+* Node.js
+
+## Building
+
+* `npm run build`
+* `npm run serve` to start
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import WebSocket from "ws";
+
+const ws_port = 8080;
+
+const wss = new WebSocket.Server({
+    port: ws_port,
+});
+
+wss.on("connection", ws => {
+    console.log("new connection");
+    ws.on("message", data => {
+        console.log(data);
+        Array.from(wss.clients.keys())
+            .filter(client => client.readyState === WebSocket.OPEN)
+            .forEach(client => {
+                client.send(data);
+                console.log("broadcasted");
+            });
+    });
+});
