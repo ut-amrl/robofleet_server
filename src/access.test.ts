8209b242561fe6cd7a4ef9895de2358ca9194bdb
Logan Zartman <logan.zartman@utexas.edu>
2020-06-16 10:33:39 -0500

    add jest+tests (failing)

---
+++ src/access.test.ts
@@ -0,0 +1,48 @@
+import { makeAuthorizer } from "./access";
+
+test("Does not authorize receive /x/y/topic for unauthorized client", () => {
+  const authorize = makeAuthorizer({
+    authorizedClients: []
+  });
+  expect(authorize({
+    ip: "1.2.3.4",
+    topic: "/x/y/topic",
+    op: "receive"
+  })).toBe(false);
+});
+
+test("Does not authorize send status for unauthorized client", () => {
+  const authorize = makeAuthorizer({
+    authorizedClients: []
+  });
+  
+  expect(authorize({
+    ip: "1.2.3.4",
+    topic: "/x/y/status",
+    op: "send"
+  })).toBe(false);
+
+  expect(authorize({
+    ip: "1.2.3.4",
+    topic: "/status",
+    op: "send"
+  })).toBe(false);
+});
+
+test("Authorizes receive status for unauthorized client", () => {
+  const authorize = makeAuthorizer({
+    authorizedClients: []
+  });
+  
+  expect(authorize({
+    ip: "1.2.3.4",
+    topic: "/status",
+    op: "receive"
+  })).toBe(true);
+
+  expect(authorize({
+    ip: "1.2.3.4",
+    topic: "/x/y/status",
+    op: "receive"
+  })).toBe(true);
+});
