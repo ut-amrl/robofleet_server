e154989208cfa20038ce00c3d43008b4bc36a3bb
Logan Zartman <logan.zartman@utexas.edu>
2020-06-16 21:29:47 -0500

    support getIp for forwaded requests

--- src/util.ts
+++ src/util.ts
@@ -2,10 +2,16 @@ import { IncomingMessage } from "http";
 import WebSocket from "ws";
 import { fb } from "./schema_generated";
 
+// get IP from IncomingMessage, with support for proxied requests
 export function getIp(req: IncomingMessage) {
   // https://github.com/websockets/ws#how-to-get-the-ip-address-of-the-client
   // https://nodejs.org/api/net.html#net_socket_remoteaddress
-  return req.socket.remoteAddress;
+  const forwardedForValues = req.headers["x-forwarded-for"];
+  if (typeof forwardedForValues === "undefined") {
+    return req.socket.remoteAddress;
+  }
+  const forwardedFor = Array.isArray(forwardedForValues) ? forwardedForValues[0] : forwardedForValues;
+  return forwardedFor.split(/\s*,\s*/)[0];
 }
 
 export function getRobofleetMetadata(data: WebSocket.Data) {
