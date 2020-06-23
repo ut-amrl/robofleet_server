1c5c455927a826a3f7cd8c6ab2711df33812201e
Logan Zartman <logan.zartman@utexas.edu>
2020-06-23 17:20:13 -0500

    setup to accept ID tokens

--- src/websocketApp.ts
+++ src/websocketApp.ts
@@ -8,6 +8,7 @@ const authorize = makeAuthorizer(config);
 const subscriptions = new SubscriptionManager();
 
 const wsIpMap = new Map<WebSocket, string>();
+const wsEmailMap = new Map<WebSocket, string>();
 
 export function setupWebsocketApp(wss: WebSocket.Server) {
   wss.on("connection", (ws, req) => {
@@ -24,44 +25,75 @@ export function setupWebsocketApp(wss: WebSocket.Server) {
     });
 
     ws.on("message", (data) => {
-      const buf = getByteBuffer(data);
-      if (buf === null) {
-        return; // received text message; ignore
-      }
-
-      // extract topic from message metadata for authorization
-      const topic = getRobofleetMetadata(buf)?.topic() ?? null;
-      if (topic === null) {
-        console.error(`WARNING: ${ip} is sending messages with no metadata`);
-        return;
-      }
-      
-      if (authorize({ip, topic, op: "send"})) {
-        // handle subscription messages
-        if (subscriptions.handleMessageBuffer(ws, buf)) {
-          return;
-        }
-
-        // broadcast any other messages
-        for (let client of wss.clients) {
-          if (client === ws)
-            continue;
-          if (client.readyState !== WebSocket.OPEN)
-            continue;
-          
-          const clientIp = wsIpMap.get(client);
-          if (clientIp === undefined) {
-            throw new Error("No IP recorded for client; wsIpMap in invalid state.");
-          }
-          
-          if (authorize({ip: clientIp, topic, op: "receive"})) {
-            if (subscriptions.isSubscribed(client, topic)) {
-              client.send(data);
-              console.log(`broadcasted ${topic} from ${ip} to ${clientIp}`);
-            }
-          }
-        }
-      }
+      handleTextMessage(wss, ws, data);
+      handleBinaryMessage(wss, ws, data);
     });
   });
+}
+
+function handleTextMessage(wss: WebSocket.Server, sender: WebSocket, data: WebSocket.Data) {
+  if (typeof data !== "string") {
+    return;
+  }
+
+  let json = null;
+  try {
+    json = JSON.parse(data);
+  } catch (e) {
+    return; // ignore malformed json
+  }
+
+  if ("id_token" in json) {
+    handleIdToken(sender, json["id_token"]);
+  }
+}
+
+function handleBinaryMessage(wss: WebSocket.Server, sender: WebSocket, data: WebSocket.Data) {
+  const buf = getByteBuffer(data);
+  if (buf === null) {
+    return;
+  }
+
+  const ip = wsIpMap.get(sender);
+  if (!ip) {
+    throw new Error("No IP recoded for sender; wsIpMap in invalid state.");
+  }
+
+  // extract topic from message metadata for authorization
+  const topic = getRobofleetMetadata(buf)?.topic() ?? null;
+  if (topic === null) {
+    console.error(`WARNING: ${ip} is sending messages with no metadata`);
+    return;
+  }
+  
+  if (authorize({ip, topic, op: "send"})) {
+    // handle subscription messages
+    if (subscriptions.handleMessageBuffer(sender, buf)) {
+      return;
+    }
+
+    // broadcast any other messages
+    for (let client of wss.clients) {
+      if (client === sender)
+        continue;
+      if (client.readyState !== WebSocket.OPEN)
+        continue;
+      
+      const clientIp = wsIpMap.get(client);
+      if (clientIp === undefined) {
+        throw new Error("No IP recorded for client; wsIpMap in invalid state.");
+      }
+      
+      if (authorize({ip: clientIp, topic, op: "receive"})) {
+        if (subscriptions.isSubscribed(client, topic)) {
+          client.send(data);
+          console.log(`broadcasted ${topic} from ${ip} to ${clientIp}`);
+        }
+      }
+    }
+  }
+}
+
+function handleIdToken(sender: WebSocket, idToken: string) {
+  console.log(`Got ID token: ${idToken}`);
 }
