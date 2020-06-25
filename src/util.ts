a8977c6fb5e194408f02c9d50a7a8a51bd316012
Logan Zartman <logan.zartman@utexas.edu>
2020-06-25 15:43:51 -0500

    improved logging

--- src/util.ts
+++ src/util.ts
@@ -3,6 +3,28 @@ import WebSocket from "ws";
 import { flatbuffers } from "flatbuffers"; // do not remove; needed by generated code
 import { fb } from "./schema_generated";
 
+export class Logger {
+  name: string;
+  loggedOnce: Set<string>;
+
+  constructor(name: string) {
+    this.name = name;
+    this.loggedOnce = new Set();
+  }
+  
+  log(msg: string) {
+    console.log(`[${this.name}] ${msg}`);
+  }
+  
+  logOnce(msg: string) {
+    if (this.loggedOnce.has(msg)) {
+      return;
+    }
+    this.log(msg);
+    this.loggedOnce.add(msg);
+  }
+}
+
 // get IP from IncomingMessage, with support for proxied requests
 export function getIp(req: IncomingMessage) {
   // https://github.com/websockets/ws#how-to-get-the-ip-address-of-the-client
--- src/websocketApp.ts
+++ src/websocketApp.ts
@@ -1,7 +1,7 @@
 import WebSocket from "ws";
 import { makeAuthorizer } from "./access";
 import config from "./config";
-import { getIp, getRobofleetMetadata, getByteBuffer } from "./util";
+import { getIp, getRobofleetMetadata, getByteBuffer, Logger } from "./util";
 import { SubscriptionManager } from "./subscriptions";
 import { OAuth2Client } from "google-auth-library"; 
 import { Socket } from "net";
@@ -14,6 +14,8 @@ const wsIpMap = new Map<WebSocket, string>();
 const oauth2Client = typeof config.clientId === "string" ? new OAuth2Client(config.clientId) : null;
 const wsEmailMap = new Map<WebSocket, string>();
 
+const wsLoggers = new Map<WebSocket, Logger>();
+
 export function setupWebsocketApp(wss: WebSocket.Server) {
   wss.on("connection", (ws, req) => {
     const ip = getIp(req);
@@ -22,10 +24,13 @@ export function setupWebsocketApp(wss: WebSocket.Server) {
       return;
     }
     wsIpMap.set(ws, ip);
-    console.log(`new connection: ${ip}`);
+    wsLoggers.set(ws, new Logger(ip));
+    console.log(`New connection: ${ip}`);
     
     ws.on("close", () => {
       wsIpMap.delete(ws);
+      wsLoggers.delete(ws);
+      console.log(`Disconnected: ${ip}`);
     });
 
     ws.on("message", (data) => {
@@ -67,7 +72,7 @@ function handleBinaryMessage(wss: WebSocket.Server, sender: WebSocket, data: Web
   // extract topic from message metadata for authorization
   const topic = getRobofleetMetadata(buf)?.topic() ?? null;
   if (topic === null) {
-    console.error(`WARNING: ${ip} is sending messages with no metadata`);
+    wsLoggers.get(sender)?.logOnce(`WARNING: ${ip} is sending messages with no metadata`);
     return;
   }
   
@@ -98,10 +103,16 @@ function handleBinaryMessage(wss: WebSocket.Server, sender: WebSocket, data: Web
       if (authorize({ip: clientIp, email: clientEmail, topic, op: "receive"})) {
         if (subscriptions.isSubscribed(client, topic)) {
           client.send(data);
-          console.log(`broadcasted ${topic} from ${ip} to ${clientIp}`);
+          wsLoggers.get(sender)?.logOnce(`broadcasting "${topic}" to ${clientIp}`);
         }
+      } else {
+        wsLoggers.get(client)?.logOnce(
+          `WARNING: client (${clientEmail ?? "not signed in"}) not authorized to receive "${topic}"`);
       }
     }
+  } else {
+    wsLoggers.get(sender)?.logOnce(
+      `WARNING: client (${senderEmail ?? "not signed in"}) not authorized to send "${topic}"`);
   }
 }
 
