593704ba556b6372f3524f8d9d281fa029e69719
Logan Zartman <logan.zartman@utexas.edu>
2020-06-26 14:10:06 -0500

    check-auth endpoint impl

--- src/expressApp.ts
+++ src/expressApp.ts
@@ -1,14 +1,44 @@
 import express from "express";
 import cors from "cors";
 import { getIp } from "./util";
-import { makeAuthorizer } from "./access";
+import { makeAuthorizer, isOp } from "./access";
 import config from "./config";
+import { googleAuthAvailable, getAuthPayload } from "./googleAuth";
 
 const authorize = makeAuthorizer(config);
 
 export function setupExpressApp(app: express.Application) {
   // use */ route to support base path
-  app.get("*/echo-ip", cors(), (req, res) => {
+
+  app.use(cors());
+
+  app.get("*/echo-ip", (req, res) => {
     res.send(getIp(req) ?? "");
+  });
+
+  app.post("*/check-auth", express.json(), async (req, res) => {
+    const ip = getIp(req);
+    
+    // get identity if token is provided
+    let email = undefined;
+    const token = req.body["id_token"];
+    if (googleAuthAvailable && typeof token === "string") {
+      const payload = await getAuthPayload(token);
+      if (payload?.email_verified && payload.email)
+        email = payload.email;
+    }
+
+    const op = req.body["op"];
+    const topic = req.body["topic"];
+
+    if (!isOp(op) || typeof topic !== "string") {
+      res.status(400).send(false);
+    } else {
+      if (authorize({ip, email, op, topic})) {
+        res.status(200).send(true);
+      } else {
+        res.status(200).send(false);
+      }
+    }
   });
 }
