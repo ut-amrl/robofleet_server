3570395afc836c27cdc5d2fdb64dbbd6e86a366e
Kavan Sikand <kvsikand@gmail.com>
2020-10-09 12:59:51 -0500

    remove mongodb, switch to level

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,29 +1,15 @@
-import Mongoose = require('mongoose');
-let database: Mongoose.Connection;
-import { RobotModel } from "./robots/robots.model";
-import RobotSchema from './robots/robots.schema';
 import config from '../config'
+import LevelTypescript from 'level-ts';
+import Level from 'level';
+import RobotInformation from './robot';
 
-export const connect = (onConnect: () => void, onError: () => void) => {
-  const uri = config.dbConnectString;
-  // add your own uri below  
-  database = Mongoose.createConnection(uri, {
-    useNewUrlParser: true,
-    useFindAndModify: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+const robotDbInstance = Level('robofleet-robots');
+let robotDatabase = new LevelTypescript<RobotInformation>(robotDbInstance);
 
-  database.once("open", onConnect);
-  database.on("error", onError);
-  database.model("robot", RobotSchema);
-
-  return database;
-};
+export function robotDb() {
+  return robotDatabase;
+}
 
 export const disconnect = () => {
-  if (!database) {
-    return;
-  }
-  Mongoose.disconnect();
+  return robotDbInstance.close();
 };
